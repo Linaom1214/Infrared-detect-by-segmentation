ed1dbab8b32649b7df937f998402188ef1aa7032
Linaom1214 <linaom1214@mail.nwpu.edu.cn>
2023-07-25 18:07:43 +0800

    update

--- main.py
+++ main.py
@@ -211,9 +211,9 @@ class App(object):
             tensor_img = tensor_img.cuda()
         pred = self.model(tensor_img)
         if torch.cuda.is_available():
-            pred = pred[0].cpu().numpy()
+            pred = pred[0][0].cpu().numpy()
         else:
-            pred = pred[0].numpy()
+            pred = pred[0][0].numpy()
         vis = mark_boundaries(np.array(image)/255., pred, color=(1, 1, 0))
         vis = Image.fromarray(np.uint8(255*vis))
         save_image(vis, 'images/pred.png')
